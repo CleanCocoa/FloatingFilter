777531601d3606904958b40bd098cd8fcc779ff5
Christian Tietze <christian.tietze@gmail.com>
2020-02-15 12:17:07 +0100

    remove title bar and make it movable by window background

--- FloatingFilter/FilterWindowController.swift
+++ FloatingFilter/FilterWindowController.swift
@@ -2,8 +2,12 @@
 
 import Cocoa
 
-class FilterWindowController: NSWindowController {
+class KeyPanel: NSPanel {
+    override var canBecomeKey: Bool { return true }
+    override var canBecomeMain: Bool { return true }
+}
 
+class FilterWindowController: NSWindowController {
     static let nibName: NSNib.Name = "FilterWindowController"
 
     @IBOutlet var filterViewController: FilterViewController!
@@ -22,8 +26,8 @@ class FilterWindowController: NSWindowController {
     
     override func windowDidLoad() {
         super.windowDidLoad()
-
-        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        guard let window = self.window else { assertionFailure(); return }
+        window.isMovableByWindowBackground = true
     }
 }
 
