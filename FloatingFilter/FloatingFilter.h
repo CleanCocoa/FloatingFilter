0137b03ed0623fa36e2eb1fa01bdb007174f9e4a
Christian Tietze <christian.tietze@gmail.com>
2020-02-15 08:17:17 +0100

    Initial Commit

---
+++ FloatingFilter/FloatingFilter.h
@@ -0,0 +1,19 @@
+//
+//  FloatingFilter.h
+//  FloatingFilter
+//
+//  Created by Christian on 15.02.20.
+//  Copyright © 2020 Christian Tietze. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FloatingFilter.
+FOUNDATION_EXPORT double FloatingFilterVersionNumber;
+
+//! Project version string for FloatingFilter.
+FOUNDATION_EXPORT const unsigned char FloatingFilterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FloatingFilter/PublicHeader.h>
+
+
